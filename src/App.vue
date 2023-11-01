202f6f581f07e9b9976db5d71dcba772265835fa
Sudhakar George <sudhakargeorge@Sudhakars-MacBook-Pro.local>
2023-11-01 16:53:17 -0400

    Added Todo Implementation

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,116 @@
 <template>
-  <v-app>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      :mobile-breakpoint="768"
+      app
+    >
+      <v-img
+        class="pa-4 pt-7"
+        src="nature.jpg"
+        height="170"
+        gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+      >
+        <v-avatar size="70" class="mb-2">
+          <img
+            src="dev.jpg"
+            alt="Sudhakar George"
+          >
+        </v-avatar>
+        <div class="white--text text-subtitle-1 font-weight-bold">
+          Sudhakar George
+        </div>
+        <div class="white--text text-subtitle-2">
+          sudhakar_george
+        </div>
+      </v-img>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          :to="item.to"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-app-bar
       app
       color="primary"
       dark
+      src="nature.jpg"
+      prominent
+      :height="$route.path === '/' ? '238' : '170'"
     >
-      <div class="d-flex align-center">
+      <template v-slot:img="{ props }">
         <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+          v-bind="props"
+          gradient="to top right, rgba(19,84,122,.9), rgba(128,208,199,.9)"
+        ></v-img>
+      </template>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+      <v-container class="header-container pa-0">
+        <v-row>
+          <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+          <v-spacer></v-spacer>
+          <search />
+        </v-row>
+        <v-row>
+          <v-toolbar-title class="text-h4 ml-4">
+            {{ $store.state.appTitle }}
+          </v-toolbar-title>
+        </v-row>
+        <v-row>
+          <live-date-time />
+        </v-row>
+        <v-row v-if="$route.path === '/'">
+          <field-add-task />
+        </v-row>
+      </v-container>
 
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <router-view></router-view>
+      <snackbar />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
-export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-};
+  export default {
+    data: () => ({
+      drawer: null,
+      items: [
+        { title: 'Todo', icon: 'mdi-format-list-checks', to: '/' },
+        { title: 'About', icon: 'mdi-help-box', to: '/about' },
+      ],
+    }),
+    mounted() {
+      this.$store.dispatch('getTasks')
+    },
+    components: {
+      'search': require('@/components/Tools/Search.vue').default,
+      'live-date-time': require('@/components/Tools/LiveDateTime.vue').default,
+      'field-add-task': require('@/components/Todo/FieldAddTask.vue').default,
+      'snackbar': require('@/components/Shared/Snackbar.vue').default
+    }
+  }
 </script>
+
+<style lang="sass">
+  .header-container
+    max-width: none
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="about pa-6">
+    <h1> About Vuetify Todo</h1>
+    <br>
+    <p>One of the most important reasons you should use a to do list is that it will help you stay organised.
+         When you write all your tasks in a list, they seem more manageable. When you've got a clear outline of the tasks you've got to do and those you've completed, it helps you stay focused.</p>
+    <br>
+    <p>This application will be like a task keeper where the user would be able to enter the tasks that they need to do.
+        Once they are done with their tasks they can also remove them from the list.</p>
+    <br>    
+    <p>Some of the eatures of this applicatio are listed below:</p>   
+     <p>
+     <ul class="pa-5">
+     <li>Create a new Task</li>   
+     <li>Edit a Task</li>
+     <li>Adding a Due Date</li>
+     <li>Deleting a Task</li>
+     <li>Rearranging Tasks</li>
+     <li>Filtering a Tasks</li>
+    </ul>
+    </p>
+   </div> 
+</template>
+<script>
+export default {
+}
+</script>
