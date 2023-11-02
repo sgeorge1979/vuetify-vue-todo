04e68a1dd82f8811996d41fac977e567ab481f4f
Sudhakar George <sudhakargeorge@Sudhakars-MacBook-Pro.local>
2023-11-02 10:32:20 -0400

    Fixed the typo

--- src/views/About.vue
+++ src/views/About.vue
@@ -8,7 +8,7 @@
     <p>This application will be like a task keeper where the user would be able to enter the tasks that they need to do.
         Once they are done with their tasks they can also remove them from the list.</p>
     <br>    
-    <p>Some of the eatures of this applicatio are listed below:</p>   
+    <p>Some of the features of this application are listed below:</p>   
      <p>
      <ul class="pa-5">
      <li>Create a new Task</li>   
